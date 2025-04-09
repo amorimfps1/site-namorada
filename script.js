c4231a98513e4006187e2b714f09094320a8ba24
amorimfps1 <90401790+amorimfps1@users.noreply.github.com>
2025-04-09 18:58:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function botao_namoro(){
+    const ama = confirm('VOCE QUER NAMORAR COMIGO MESMO?')
+    if (ama){
+        alert('EBAAAAAAAAAAAAAAAAAAAA')
+    }
+    else{
+        alert('vc nao quer mas eu quero aff')
+    }
+}
+function recado(){
+    const recado = prompt('Oi escreva seu recado: ')
+
+}
